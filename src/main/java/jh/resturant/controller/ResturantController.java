ef9f64855f26c2db33ea0d0fa2c47d375b18f86b
wogns4004 <wogns4004@naver.com>
2023-02-03 17:49:59 +0900

    공공데이터 api

--- src/main/java/jh/resturant/controller/ResturantController.java
+++ src/main/java/jh/resturant/controller/ResturantController.java
@@ -1,5 +1,10 @@
 package jh.resturant.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -76,5 +81,35 @@ public class ResturantController {
 		ResturantVO vo = resturantMapper.goDetail(businessNumber);
 		model.addAttribute("vo",vo);
 		return "detail";
+	}
+	
+	@RequestMapping("shop.do")
+	public String shop() throws Exception{
+		 StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6290000/eventbaseinfo/geteventbaseinfo"); /*URL*/
+	        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "nV%2F4ELjExxo%2F%2B8iWVesr3uW3dNvYxJnqx6PrMv8Set%2FwYHkl6RmAwPjjTdmYp7T1BbOt9HxA7gV8%2BCHHrvuH8g%3D%3D"); /*Service Key*/
+	        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("2", "UTF-8")); /*한 페이지 결과 수*/
+	        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+	        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*응답 받을 데이터 형식*/
+	        urlBuilder.append("&" + URLEncoder.encode("eventNm","UTF-8") + "=" + URLEncoder.encode("뮤지컬", "UTF-8")); /*행사명*/
+	        URL url = new URL(urlBuilder.toString());
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Content-type", "application/json");
+	        System.out.println("Response code: " + conn.getResponseCode());
+	        BufferedReader rd;
+	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        } else {
+	            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+	        }
+	        StringBuilder sb = new StringBuilder();
+	        String line;
+	        while ((line = rd.readLine()) != null) {
+	            sb.append(line);
+	        }
+	        rd.close();
+	        conn.disconnect();
+	        System.out.println(sb.toString());
+		return "main";
 	}
 }
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -26,6 +26,7 @@
                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                      <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                      <li class="nav-item"><a class="nav-link" href="list.do">List</a></li>
+                     <li class="nav-item"><a class="nav-link" href="shop.do">api</a></li>
                  </ul>
              </div>
          </div>
